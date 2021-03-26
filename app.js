77ae0d6292060180e81cf7fffae92aa502d7ee10
taisiia-filikova <filikova@gmail.com>
2021-03-27 00:20:37 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+const helmet = require("helmet");
+const path=require('path')
+const rateLimit = require("express-rate-limit");
+const { HttpCode } = require('./helpers/constants')
+
+const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users')
+require('dotenv').config()
+
+const app = express()
+
+const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS
+app.use(express.static(path.join(__dirname,AVATARS_OF_USERS)))
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(helmet());
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json({limit:10000}))
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  handler: (req, res, next) => {
+    return res.status(HttpCode.BAD_REQUEST).json({
+      status: 'error',
+      code: HttpCode.BAD_REQUEST,
+      data: 'Forbidden',
+      message: 'Too many requests,please try later',
+    })
+  }
+});
+
+app.use("/api/", apiLimiter);
+
+app.use('/api/users', usersRouter)
+app.use('/api/contacts', contactsRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message })
+})
+
+module.exports = app
